2ab9210347d7e9289970172e2b0b3bc655f357ff
Abhishek Kumar Mehta <akmehta7252@gmail.com>
2024-03-19 19:17:43 +0530

    new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.css 	new file:   src/App.jsx 	new file:   src/assets/assets.js 	new file:   src/assets/bulb_icon.png 	new file:   src/assets/code_icon.png 	new file:   src/assets/compass_icon.png 	new file:   src/assets/gallery_icon.png 	new file:   src/assets/gallery_icon2.png 	new file:   src/assets/gemini_icon.png 	new file:   src/assets/history_icon.png 	new file:   src/assets/menu_icon.png 	new file:   src/assets/message_icon.png 	new file:   src/assets/mic_icon.png 	new file:   src/assets/plus_icon.png 	new file:   src/assets/question_icon.png 	new file:   src/assets/send_icon.png 	new file:   src/assets/setting_icon.png 	new file:   src/assets/user_icon.png 	new file:   src/assets/youtube_icon.png 	new file:   src/components/Main/Main.css 	new file:   src/components/Main/Main.jsx 	new file:   src/components/Sidebar/Sidebar.css 	new file:   src/components/Sidebar/Sidebar.jsx 	new file:   src/config/gemini.js 	new file:   src/context/Context.jsx 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   vite.config.js

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,142 @@
+import { useContext } from "react";
+import { assets } from "../../assets/assets";
+import "./Main.css";
+import { Context } from "../../context/Context";
+
+const Main = () => {
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      onSent();
+    }
+  };
+
+  const handleCardClick = (prompt) => {
+    setInput(prompt);
+    recentPrompt(prompt);
+    onSent();
+  };
+
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello, Dev</span>
+              </p>
+              <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+              <div
+                className="card"
+                onClick={() =>
+                  handleCardClick(
+                    "How can I list all processes that have been running longer than an hour in linux?"
+                  )
+                }
+              >
+                <p>
+                  How can I list all processes that have been running longer
+                  than an hour in linux?
+                </p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
+              <div
+                className="card"
+                onClick={() =>
+                  handleCardClick(
+                    "Briefly summarize this concept: urban planning"
+                  )
+                }
+              >
+                <p>Briefly summarize this concept: urban planning</p>
+                <img src={assets.bulb_icon} alt="" />
+              </div>
+              <div
+                className="card"
+                onClick={() =>
+                  handleCardClick(
+                    "Brainstorm team bonding activities for our work retreat"
+                  )
+                }
+              >
+                <p>Brainstorm team bonding activities for our work retreat</p>
+                <img src={assets.message_icon} alt="" />
+              </div>
+              <div
+                className="card"
+                onClick={() =>
+                  handleCardClick(
+                    "Tell me about Firebase how i can use it in my project"
+                  )
+                }
+              >
+                <p>Tell me about Firebase how i can use it in my project</p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <img src={assets.user_icon} alt="" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
+            </div>
+          </div>
+        )}
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              onKeyDown={handleKeyDown}
+              value={input}
+              type="text"
+              placeholder="Enter a prompt here"
+            />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              {input ? (
+                <img onClick={() => onSent()} src={assets.send_icon} alt="" />
+              ) : null}
+            </div>
+          </div>
+          <p className="bottom-info">
+            Gemini may display inaccurate info, including about people, so
+            double-check its responses. Your privacy & Gemini Apps
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
